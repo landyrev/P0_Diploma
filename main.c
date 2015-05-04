9a2b3864b5896a2f9f3d1f22c0dfa8a1aa325db4
Akexey Landyrev <landyrevag@gmail.com>
2015-05-04 20:04:56 +0300

    Updates in main()

--- main.c
+++ main.c
@@ -1,46 +1,17 @@
-#include <msp430.h> 
 #include <stdio.h>
-
-void delay(unsigned int ms);
+#include <init.h>
+#include <uart.h>
+//#include <cardreader.h>
 
 int main(void)  //Main program
 {
-   WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
+	deviceInit();
+	puts("Shiet");
 
-   CSCTL0_H = 0xA5; //Control word
-   CSCTL3 = 0;	//Set all dividers to 0
-   CSCTL3 |= DIVS0 | DIVS2; //Set SMCLK Divider to 8
+	//initCardReader();
+	initUART();
 
-   P1DIR &= ~BIT3; //P1.3 Direction IN
-   PJDIR |= BIT1; //LED1 Direction OUT
+	__bis_SR_register(GIE); // Enable all interrupts
 
-   PJOUT &= ~BIT1; //LED1 Off
-
-   P1IES |= BIT3; 	//P1.3 Rising edge Interrupt
-   P1IFG &= ~BIT3;	//Clear P1.3 Interrupt Flag
-   P1IE |= BIT3; 	//Enable P1.3 Interrupt
-
-   __bis_SR_register(GIE); // Enable all interrupts
-
-   while(1)
-   {
-	   PJOUT ^= BIT1;
-	   delay(1000);
-	   PJOUT ^= BIT1;
-	   delay(1000);
-   }
-   __no_operation();
-}
-
-#pragma vector=PORT1_VECTOR
-__interrupt void P1INT() // Обработчик прерывания
-{
-	P1IE &= ~BIT3; // Запрет прерываний на P1.3
-	//Begin of user code
-	PJOUT |= BIT1;
-	__delay_cycles(100);
-	PJOUT &= ~BIT1;
-	//End of user code
-	P1IE |= BIT3; // Разрешение прерываний на P1.2
-	P1IFG &= ~BIT3; // Очистка флага прерываний для P1.2
+	__no_operation();
 }
