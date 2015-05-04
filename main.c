3a25700e427828f9bef8f21cc1bf51e206735a8d
Akexey Landyrev <landyrevag@gmail.com>
2015-05-04 23:27:35 +0300

    Some minor main changes

--- main.c
+++ main.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <init.h>
 #include <uart.h>
-//#include <cardreader.h>
+#include <delay.h>
+#include <cardreader.h>
 
 int main(void)  //Main program
 {
 	deviceInit();
-	puts("Shiet");
 
-	//initCardReader();
+	initCardReader();
 	initUART();
 
 	__bis_SR_register(GIE); // Enable all interrupts
+
+	while(1)
+	{
+		UARTsendChar('a');
+		UARTsendChar('b');
+		UARTsendChar('c');
+		UARTsendChar('d');
+		__delay_cycles(100000);
+	}
 
 	__no_operation();
 }
