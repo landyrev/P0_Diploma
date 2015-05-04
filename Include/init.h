0ad7a301b40d16b7273eadc8c7fcc9e4600f7880
Akexey Landyrev <landyrevag@gmail.com>
2015-05-04 20:03:51 +0300

    Init Lib

---
+++ Include/init.h
@@ -0,0 +1,4 @@
+#define SMCLK 250000
+#include <msp430.h>
+
+int deviceInit();
---
+++ Src/init.c
@@ -0,0 +1,13 @@
+#include <msp430.h>
+#include <init.h>
+
+int deviceInit()
+{
+	   WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
+
+	   //Setting Clock
+	   CSCTL0_H = 0xA5; //Control word
+	   CSCTL3 = 0;	//Set all dividers to 0
+	   CSCTL3 |= DIVS0 | DIVS2; //Set SMCLK Divider to 8
+	   return 0;
+}
