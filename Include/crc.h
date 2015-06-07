a913e8cff4a00ae52a5fff5d0fe17e08d54cfac4
Akexey Landyrev <landyrevag@gmail.com>
2015-06-07 23:12:53 +0300

    New CRC Library

---
+++ Include/crc.h
@@ -0,0 +1,9 @@
+﻿/*
+ * crc.h
+ *
+ *  Created on: 07 θών 2015 γ.
+ *      Author: landyrev
+ */
+
+unsigned char Crc8(unsigned char *pcBlock, unsigned char len);
+unsigned short Crc16(unsigned char *pcBlock, unsigned char len);
