a962f86d119433475fdf3519964f0e28ef6afa70
Akexey Landyrev <landyrevag@gmail.com>
2015-06-07 23:09:39 +0300

    Delay and Uart now using LPM0

--- Src/delay.c
+++ Src/delay.c
@@ -8,20 +8,30 @@
 #include <delay.h>
 #include <init.h>
 
+int flag;
+
 void delay_micro(unsigned int micros)
 {
 	TA0CTL = 0;
 	TA0CCR0=micros;
-	TA0CTL |= TASSEL1 | MC0 | TACLR;
-	while (!(TA0CTL&TAIFG));
+	TA0CTL |= TASSEL1 | MC0 | TAIE | TACLR;
+	__bis_SR_register(GIE + LPM0_bits);
 	TA0CTL = 0;
 }
 
 void delay(unsigned int millis)
 {
+	flag=0;
 	TA0CTL = 0;
 	TA0CCR0=millis*125; //125 = (SMCLK/8)/1000 - количество тактов в одну мс.
-	TA0CTL |= TASSEL1 | ID0 | ID1 | MC0 | TACLR;
-	while (!(TA0CTL&TAIFG));
+	TA0CTL |= TASSEL1 | ID0 | ID1 | MC0 | TAIE | TACLR;
+	__bis_SR_register(GIE + LPM0_bits);
 	TA0CTL = 0;
+}
+
+#pragma vector=TIMER0_A1_VECTOR
+__interrupt void TA0_ISR()
+{
+	__bic_SR_register_on_exit(LPM0_bits);
+	TA0CTL &= ~ TAIFG;
 }
--- Src/uart.c
+++ Src/uart.c
@@ -6,7 +6,6 @@
  */
 #include <init.h>
 #include <uart.h>
-#include <stdio.h>
 
 void initUART()
 {
@@ -21,12 +20,14 @@ void initUART()
 	// Configure UART 0
 	UCA0CTL1 |= UCSWRST;
 	UCA0CTL1 = UCSSEL1;                      // Set SMCLK = 250000 as UCBRCLK
-	UCA0BR0 = 26;                              // 9600 baud
+	UCA0BR0 = 6;                              // 9600 baud
 	UCA0BR1 = 0;
-	UCA0MCTLW |= 0x0000;                  // 250000/9600 - INT(250000/9600)=0.41
-	// UCBRSx value = 0x53 (See UG)
+	UCA0MCTLW=0;
+	UCA0MCTLW_L |= UCBRF3_L;				//UCBRFx = 8(0b1000)
+	UCA0MCTLW_H |= 0x20;					//UCBRSx = 0x20
+	UCA0MCTLW_L |= UCOS16_L;				//UCOS16=1;
+
 	UCA0CTL1 &= ~UCSWRST;                     // release from reset
-	UCA0IE |= UCRXIE;                         // Enable RX interrupt
 }
 
 void UARTsemSet()
@@ -40,13 +41,13 @@ void UARTsemLeave()
 	UARTsemaphore++;
 }
 
-void UARTsendChar (char ch)
+void UARTsendChar (unsigned char ch)
 {
 	while (!(UCA0IFG&UCTXIFG));             // USCI_A0 TX buffer ready?
 	UCA0TXBUF_L=ch;
 }
 
-void UARTsend(const char *_ptr)
+void UARTsend(const unsigned char *_ptr)
 {
 	unsigned short i=0;
 	while (_ptr[i])
@@ -55,65 +56,53 @@ void UARTsend(const char *_ptr)
 	UARTsendChar(0x0A);
 }
 
-void UARTsendID(const char *_id, const unsigned short number)
+void UARTsendID(const unsigned char *_id, const unsigned short number)
 {
 	UARTsemSet();
 	unsigned short i=0;
 	while (_id[i])
 		UARTsendChar(_id[i++]);
 	UARTsendChar(':');
-	UARTsendChar((char)(number>>8));
-	UARTsendChar((char)number);
+	UARTsendChar((unsigned char)(number>>8));
+	UARTsendChar((unsigned char)number);
 	UARTsendChar(0x0D);
 	UARTsendChar(0x0A);
 	UARTsemLeave();
 }
 
-// Echo back RXed character, confirm TX buffer is ready first
-#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=USCI_A0_VECTOR
-__interrupt void USCI_A0_ISR(void)
-#elif defined(__GNUC__)
-void __attribute__ ((interrupt(USCI_A0_VECTOR))) USCI_A0_ISR (void)
-#else
-#error Compiler not supported!
-#endif
+void UARTsendLen(const unsigned char *_data, unsigned short length)
 {
-	volatile char x;
-	switch(__even_in_range(UCA0IV,0x08))
-	{
-	case 0:break;                             // Vector 0 - no interrupt
-	case 2:                                   // Vector 2 - RXIFG
-		x=UCA0RXBUF-0x30;
-		UARTsemSet();
-		switch (x)
-		{
-		case 0:
-			PJOUT ^= BIT0;
-			if (PJOUT&BIT0) UARTsend("lamp0:1"); else UARTsend("lamp0:0");
-			break;
-		case 1:
-			PJOUT ^= BIT1;
-			if (PJOUT&BIT1) UARTsend("lamp1:1"); else UARTsend("lamp1:0");
-			break;
-		case 2:
-			PJOUT ^= BIT2;
-			if (PJOUT&BIT2) UARTsend("lamp2:1"); else UARTsend("lamp2:0");
-			break;
-		case 3:
-			PJOUT ^= BIT3;
-			if (PJOUT&BIT3) UARTsend("lamp3:1"); else UARTsend("lamp3:0");
-			break;
-		case 7:
-			if (PJOUT&BIT0) UARTsend("lamp0:1"); else UARTsend("lamp0:0");
-			if (PJOUT&BIT1) UARTsend("lamp1:1"); else UARTsend("lamp1:0");
-			if (PJOUT&BIT2) UARTsend("lamp2:1"); else UARTsend("lamp2:0");
-			if (PJOUT&BIT3) UARTsend("lamp3:1"); else UARTsend("lamp3:0");
-		default: break;
-		}
-		break;
-		case 4:break;                             // Vector 4 - TXIFG
-		default: break;
-	}
+	UARTsemSet();
+	while (length--)
+		UARTsendChar(*_data++);
 	UARTsemLeave();
+}
+
+int UARTRecieveMicro(unsigned int micros, unsigned char* c, int count)
+{
+	int i;
+	for (i=0; i<count; i++)
+	{
+		if (UARTgetCharMicro(micros, &c[i]))
+			return 1;
+	}
+	return 0;
+}
+
+int UARTgetCharMicro(unsigned int micros, unsigned char* c)
+{
+	TA0CTL = 0;
+	TA0CCR0=micros;
+	TA0CTL |= TASSEL1 | MC0 | TACLR;
+	while (!( (TA0CTL&TAIFG)||(UCA0IFG&UCRXIFG) ));
+	TA0CTL = 0;
+	if (UCA0IFG&UCRXIFG)
+	{
+		*c=UCA0RXBUF;
+		return 0;
+	} else
+	{
+		*c=0;
+		return 1;
+	}
 }
